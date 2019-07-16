9fedc2ba79bcf57df97038bd8adcaf2160858b29
tuan anh - ti <t.t.anh@alive-web.co.jp>
2019-07-16 14:26:41 +0700

    [Tuan] modify

--- dist/danh-sach-san-pham/index.php
+++ dist/danh-sach-san-pham/index.php
@@ -3,7 +3,7 @@ $thisPageName = 'thuonghieu';
 include_once('../app_config.php');
 include(APP_PATH.'libs/head.php');
 ?>
-<link rel="stylesheet" href="<?php echo APP_ASSETS ?>css/page/thuonghieu.min.css">
+<link rel="stylesheet" href="<?php echo APP_ASSETS ?>css/page/sanpham.min.css">
 </head>
 <body class='products'>
 
@@ -32,8 +32,90 @@ include(APP_PATH.'libs/head.php');
   </div> <!-- #wrap -->
   <div class="container">
       <div class="row">
-          <div class="products__box">
-              <div class="products__box__around"></div>
+          <div class="products__list">
+              <div class="products__around">
+                    <a href="#" class="products__box">
+                        <div class="products__box__thumb">
+                            <img src="<?php echo APP_ASSETS; ?>img/sanpham/san-pham-01.png" alt=""/>
+                       </div>
+                      <h3 class="products__box__tit">Oxy Siêu Dưỡng Nano Complex</h3>
+                    </a>
+                    <a href="#" class="products__box">
+                        <div class="products__box__thumb">
+                            <img src="<?php echo APP_ASSETS; ?>img/sanpham/san-pham-02.png" alt=""/>
+                       </div>
+                      <h3 class="products__box__tit">Oxy Siêu Dưỡng Nano Complex</h3>
+                    </a>
+                    <a href="#" class="products__box">
+                        <div class="products__box__thumb">
+                            <img src="<?php echo APP_ASSETS; ?>img/sanpham/san-pham-03.png" alt=""/>
+                       </div>
+                      <h3 class="products__box__tit">Oxy Siêu Dưỡng Nano Complex</h3>
+                    </a>
+                    <a href="#" class="products__box">
+                        <div class="products__box__thumb">
+                            <img src="<?php echo APP_ASSETS; ?>img/sanpham/san-pham-04.png" alt=""/>
+                       </div>
+                      <h3 class="products__box__tit">Oxy Siêu Dưỡng Nano Complex</h3>
+                    </a>
+                    <a href="#" class="products__box">
+                        <div class="products__box__thumb">
+                            <img src="<?php echo APP_ASSETS; ?>img/sanpham/san-pham-01.png" alt=""/>
+                       </div>
+                      <h3 class="products__box__tit">Oxy Siêu Dưỡng Nano Complex</h3>
+                    </a>
+                    <a href="#" class="products__box">
+                        <div class="products__box__thumb">
+                            <img src="<?php echo APP_ASSETS; ?>img/sanpham/san-pham-02.png" alt=""/>
+                       </div>
+                      <h3 class="products__box__tit">Oxy Siêu Dưỡng Nano Complex</h3>
+                    </a>
+                    <a href="#" class="products__box">
+                        <div class="products__box__thumb">
+                            <img src="<?php echo APP_ASSETS; ?>img/sanpham/san-pham-03.png" alt=""/>
+                       </div>
+                      <h3 class="products__box__tit">Oxy Siêu Dưỡng Nano Complex</h3>
+                    </a>
+                    <a href="#" class="products__box">
+                        <div class="products__box__thumb">
+                            <img src="<?php echo APP_ASSETS; ?>img/sanpham/san-pham-04.png" alt=""/>
+                       </div>
+                      <h3 class="products__box__tit">Oxy Siêu Dưỡng Nano Complex</h3>
+                    </a>
+                    <a href="#" class="products__box">
+                        <div class="products__box__thumb">
+                            <img src="<?php echo APP_ASSETS; ?>img/sanpham/san-pham-01.png" alt=""/>
+                       </div>
+                      <h3 class="products__box__tit">Oxy Siêu Dưỡng Nano Complex</h3>
+                    </a>
+                    <a href="#" class="products__box">
+                        <div class="products__box__thumb">
+                            <img src="<?php echo APP_ASSETS; ?>img/sanpham/san-pham-02.png" alt=""/>
+                       </div>
+                      <h3 class="products__box__tit">Oxy Siêu Dưỡng Nano Complex</h3>
+                    </a>
+                    <a href="#" class="products__box">
+                        <div class="products__box__thumb">
+                            <img src="<?php echo APP_ASSETS; ?>img/sanpham/san-pham-03.png" alt=""/>
+                       </div>
+                      <h3 class="products__box__tit">Oxy Siêu Dưỡng Nano Complex</h3>
+                    </a>
+                    <a href="#" class="products__box">
+                        <div class="products__box__thumb">
+                            <img src="<?php echo APP_ASSETS; ?>img/sanpham/san-pham-04.png" alt=""/>
+                       </div>
+                      <h3 class="products__box__tit">Oxy Siêu Dưỡng Nano Complex</h3>
+                    </a>
+              </div>
+              <div class="wp-pagenavi"> 
+                <a class="prev page-numbers" href="javascript:void(0);"></a>
+                <a class="page-numbers" href="javascript:void(0);">1</a>
+                <a class="page-numbers" href="javascript:void(0);">2</a>
+                <span class="page-numbers current">3</span>
+                <a class="page-numbers" href="javascript:void(0);">4</a>
+                <a class="page-numbers" href="javascript:void(0);">5</a>
+                <a class="next page-numbers" href="javascript:void(0);"></a>                    
+                </div>
           </div>
       </div>
   </div>
